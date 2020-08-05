3cc913e55f057259fae5d334df8d60e3bed4acbb
zk <zhangkang@dbg.com.cn>
2020-08-05 13:46:44 +0800

    '完成了登录功能'

---
+++ src/components/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+      sss
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import Login from '../components/Login.vue'
+import Index from '../components/Index.vue'
+import Home from '../components/Home.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// eslint-disable-next-line no-undef
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
 
-const router = new VueRouter({
-  routes
+//挂z载路由导航守卫
+router.beforeEach((to, from, next) => {
+  //to代表将要访问的路径
+  //from代表从哪个路径跳转而来
+  //next表示放行，是一个函数，两种方式（1）next():直接放行（2）next('/login')：强制跳转
+
+  if(to.path === '/login') return next();
+  const tokenStr =  window.sessionStorage.getItem('token')
+  if(!tokenStr) return next('/login')
+  next()
 })
 
 export default router
